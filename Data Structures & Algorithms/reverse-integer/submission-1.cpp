b6126b973ad48fec02688f2912df9ca7f8972cf3
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-04-12 12:54:59 -0400

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        int digit = 0;
+        while (x != 0) {
+            digit = x % 10;
+            x /= 10;
+
+            // Keypoint: how to check overflow!!
+            if (res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > 7))
+                return 0;
+            if (res < INT_MIN / 10 || (res == INT_MIN / 10 && digit < -8))
+                return 0;
+            
+            res = res * 10 + digit;
+        }
+        return res;
+    }
+};
