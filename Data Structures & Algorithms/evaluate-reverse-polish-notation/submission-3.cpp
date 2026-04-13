d6956d8e59cd1cde417e8482e273c08fbd3b5f49
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-04-13 00:08:42 -0400

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        // Key: Definition of Reverse Polish Notation
+
+        stack<int> operand_stack;
+        for (string token: tokens) {
+            if (token == "+") {
+                int first = operand_stack.top();
+                operand_stack.pop();
+                int second = operand_stack.top();
+                operand_stack.pop();
+                operand_stack.push(first + second);
+            } else if (token == "-") {
+                int first = operand_stack.top();
+                operand_stack.pop();
+                int second = operand_stack.top();
+                operand_stack.pop();
+                operand_stack.push(second - first);
+            } else if (token == "*") {
+                int first = operand_stack.top();
+                operand_stack.pop();
+                int second = operand_stack.top();
+                operand_stack.pop();
+                operand_stack.push(second * first);
+            } else if (token == "/") {
+                int first = operand_stack.top();
+                operand_stack.pop();
+                int second = operand_stack.top();
+                operand_stack.pop();
+                operand_stack.push(second / first);
+            } else {
+                operand_stack.push(stoi(token));
+            }
+        }
+        
+        return operand_stack.top();
+    }
+};
