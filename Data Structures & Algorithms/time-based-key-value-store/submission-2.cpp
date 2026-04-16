b68dee43928a28cd8c2510d82cd0075f306dcb92
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-04-15 23:42:13 -0400

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cpp
@@ -0,0 +1,50 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<tuple<int, string>>> hash_map;
+
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        if (hash_map.find(key) == hash_map.end()) {
+            // create new vector
+            hash_map[key] = vector<tuple<int, string>>();
+        }
+        hash_map[key].push_back(make_tuple(timestamp, value));
+    }
+    
+    string get(string key, int timestamp) {
+        // binary search
+        // For finding largest smaller or smallest larger,
+        //  we must check (store) the value first before assigning
+        //        half + 1 or half -1
+        //       otherwise, the valid value might be excluded!!
+
+        if (hash_map.find(key) == hash_map.end()) {
+            return "";
+        }
+
+        int left = 0;
+        int right = hash_map[key].size() - 1;
+        auto &vec_ref = hash_map[key];
+        while (left < (right-1)) {
+            int half = (left + right) / 2;
+
+            if (get<0>(vec_ref[half]) == timestamp) {
+                return get<1>(vec_ref[half]);
+            } else if (get<0>(vec_ref[half]) < timestamp) {
+                left = half;
+            } else {
+                right = half - 1;
+            }
+        }
+
+        if (get<0>(vec_ref[right]) <= timestamp) {
+            return get<1>(vec_ref[right]);
+        } else if (get<0>(vec_ref[left]) <= timestamp) {
+            return get<1>(vec_ref[left]);
+        }
+
+        return "";
+    }
+};
