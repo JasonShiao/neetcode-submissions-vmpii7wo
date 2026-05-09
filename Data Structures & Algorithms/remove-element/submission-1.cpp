836a679e8d6390c2f0fe4ec68d5913019220796d
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-09 18:43:15 -0400

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // search non-val by non_val_cur (which moves at least as fast as cur)
+        // move the non-val to cur
+
+        int cur = 0; // cursor to place the next non-val
+        int non_val_cur = 0; // point to the first non-val which is not processed yet
+        while (non_val_cur < nums.size()) {
+            if (nums[non_val_cur] == val) {
+                non_val_cur += 1;
+                continue;
+            }
+            nums[cur] = nums[non_val_cur];
+            cur += 1;
+            non_val_cur += 1;
+        }
+
+        return cur;
+    }
+};
