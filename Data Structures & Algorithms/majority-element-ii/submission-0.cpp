dee603264c54d58630093808f5d28ea63328f164
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-12 14:56:29 -0400

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        // maintain two candidate and counter
+        int cand1 = 0;
+        int cand2 = 0;
+        int cand1_cnt = 0;
+        int cand2_cnt = 0;
+        // Find the two most common cand (not guaranteed to > n/3)
+        for (auto& num: nums) {
+            if (num == cand1) {
+                cand1_cnt += 1;
+            } else if (num == cand2) {
+                cand2_cnt += 1;
+            } else if (cand1_cnt == 0) {
+                cand1 = num;
+                cand1_cnt = 1;
+            } else if (cand2_cnt == 0) {
+                cand2 = num;
+                cand2_cnt = 1;
+            } else {
+                cand1_cnt -= 1;
+                cand2_cnt -= 1;
+            }
+        }
+        // Verify: check whether > n/3
+        vector<int> res;
+        cand1_cnt = 0;
+        cand2_cnt = 0;
+        for (auto& num: nums) {
+            if (num == cand1) {
+                cand1_cnt++;
+            } else if (num == cand2) {
+                cand2_cnt++;
+            }
+        }
+        if (cand1_cnt > nums.size() / 3) {
+            res.push_back(cand1);
+        }
+        if (cand2_cnt > nums.size() / 3) {
+            res.push_back(cand2);
+        }
+
+        return res;
+    }
+};
