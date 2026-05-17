07d6e6fca7bfc8e19db6f854780ccee5a2bade3e
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-17 17:59:07 -0400

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        // (Intuitive) Approach 1: hash table with ListNode addr as key
+        // require extra space O(n)
+        // unordered_map<ListNode*, bool> lookup;
+        // ListNode* cur = head;
+        // while (cur != nullptr) {
+        //     if (lookup.find(cur) != lookup.end()) {
+        //         return true;
+        //     }
+        //     lookup[cur] = true;
+        //     cur = cur->next;
+        // }
+
+        // Approach 2: fast and slow pointer cycle detection
+        ListNode* fast_ptr = head;
+        ListNode* slow_ptr = head;
+        while (fast_ptr != nullptr && fast_ptr->next != nullptr) {
+            fast_ptr = fast_ptr->next->next;
+            slow_ptr = slow_ptr->next;
+            if (fast_ptr == slow_ptr) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
