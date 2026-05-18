960f819bf24c1d892d2e1bdb19d332b4c587ae46
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-18 18:34:46 -0400

    Add: online-stock-span - submission-1

---
+++ Data Structures & Algorithms/online-stock-span/submission-1.cpp
@@ -0,0 +1,35 @@
+class StockSpanner {
+    stack<pair<int, int>> price_span_stack;
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        // Tricks: "compress" the price span
+        //         A high price (with span) summarizes the previous low prices
+        int span = 1; // including itself
+
+        while (!price_span_stack.empty()) {
+            if (price >= price_span_stack.top().first) {
+                span += price_span_stack.top().second;
+                price_span_stack.pop();
+            } else {
+                price_span_stack.push(make_pair(price, span));
+                break;
+            }
+        }
+
+        if (price_span_stack.empty()) {
+            price_span_stack.push(make_pair(price, span));
+        }
+
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
