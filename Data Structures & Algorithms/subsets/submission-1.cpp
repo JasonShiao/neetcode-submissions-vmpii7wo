c8e8505c02b802138ef0a6e8e0e4f2ee10c2f683
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-22 17:52:19 -0400

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void backtrack(vector<int>& nums, int idx,
+                   vector<int>& curr,
+                   vector<vector<int>>& res) {
+        if (idx == nums.size()) {
+            res.push_back(curr);
+            return;
+        }
+
+        // exclude nums[idx]
+        backtrack(nums, idx + 1, curr, res);
+
+        // include nums[idx]
+        curr.push_back(nums[idx]);
+        backtrack(nums, idx + 1, curr, res);
+        curr.pop_back(); // backtrack
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> curr;
+
+        backtrack(nums, 0, curr, res);
+
+        return res;
+    }
+};
