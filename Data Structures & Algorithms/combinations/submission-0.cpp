89fa3fb1932cfe5a73ef219ff7dfe428cf921abe
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-22 18:51:04 -0400

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void backtrack(const int& n, int i, 
+            int cnt, const int& k,
+            vector<vector<int>>& res, 
+            vector<int>& cur_combination) {
+        if (cnt == k) { // complete a combination
+            res.push_back(cur_combination);
+            return;
+        }
+        if (i > n) {
+            return;
+        }
+
+        // exclude i
+        backtrack(n, i + 1, cnt, k, res, cur_combination);
+        // include i
+        cur_combination.push_back(i);
+        backtrack(n, i + 1, cnt + 1, k, res, cur_combination);
+        cur_combination.pop_back();
+
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> cur_combination;
+        backtrack(n, 1, 0, k, res, cur_combination);
+        
+        return res;
+    }
+};
