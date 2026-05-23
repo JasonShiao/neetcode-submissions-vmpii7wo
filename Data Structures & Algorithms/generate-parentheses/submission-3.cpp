e9ad58d233cd42ace6a3b0baedd220369a387e18
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-22 23:56:10 -0400

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void backtrack(int n, int open_remain, vector<string>& res, string s) {
+        // if (n - s.length() < open_remain) {
+        //     return;
+        // }
+        if (s.length() == n) {
+            if (open_remain == 0) { // NOTICE: add to res only when valid
+                res.push_back(s);
+            }
+            return;
+        }
+
+        if (open_remain > 0) {
+            // can add ) close paranthesis
+            backtrack(n, open_remain - 1, res, s + ')');
+        }
+        backtrack(n, open_remain + 1, res, s + '(');
+
+    }
+    vector<string> generateParenthesis(int n) {
+        // Key: At each position, we could append open ( or close )
+
+        vector<string> res;
+
+        backtrack(n * 2, 0, res, "");
+
+        return res;
+    }
+};
