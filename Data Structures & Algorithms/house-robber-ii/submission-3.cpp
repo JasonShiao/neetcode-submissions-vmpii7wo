20cfdeb77fb7ff6ce577cf2d2e23dc5c26cbf534
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-23 11:26:43 -0400

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int robLinear(vector<int>& nums, int l, int r) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        // Compare select and not select i
+        for (int i = l; i <= r; i++) {
+            int cur = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return prev1;
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        // separate two cases:
+        return max(
+            robLinear(nums, 0, n - 2), // include first, exclude last
+            robLinear(nums, 1, n - 1)  // exclude first, include last
+        );
+    }
+};
