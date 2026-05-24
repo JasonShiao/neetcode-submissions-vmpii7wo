8eef246d5a26e0a6dde4ebfecb2fa26c3da5a177
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-24 17:41:22 -0400

    Add: capacity-to-ship-packages-within-d-days - submission-2

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-2.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        // binary search
+        // for each trial, test the required days
+
+        // notice the cand must > max(weights)
+
+        // key: determine the upperbound for binary search
+        // intuitive: sum(weights)
+        int l = 0; // at least the max(weights)
+        int r = 0; // upperbound: sum(weights)
+        for (auto w: weights) {
+            r += w;
+            if (w > l) { 
+                l = w;
+            }
+        }
+
+        int cand_capacity;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            // calculate required days
+            int days_req = 0;
+            int day_weight_cum = 0;
+            // note: Load on the conveyor belt cannot change order!!
+            for (auto w: weights) {
+                if (day_weight_cum + w > mid) {
+                    // cut
+                    days_req += 1;
+                    day_weight_cum = w;
+                } else {
+                    day_weight_cum += w;
+                }
+            }
+            if (day_weight_cum > 0) {
+                days_req += 1;
+            }
+            // check condition
+            if (days_req <= days) { // valid answer -> store and keep finding the "least"
+                r = mid - 1;
+                cand_capacity = mid;
+            } else { // not a valid answer
+                l = mid + 1;
+            }
+        }
+
+        return cand_capacity;
+
+    }
+};
