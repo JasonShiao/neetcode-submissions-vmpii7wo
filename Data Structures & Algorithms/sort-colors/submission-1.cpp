79c236a992134693070a080fe747e874c1058734
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-24 14:59:02 -0400

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        // Bucket sort's best use case: the distinct values in array isn't many
+
+        vector<int> bucket{0, 0, 0};
+
+        for (auto& num: nums) {
+            bucket[num] += 1;
+        }
+
+        int cur = 0;
+        for (auto i = 0; i < bucket.size(); i++) {
+            for (auto j = 0; j < bucket[i]; j++) {
+                nums[cur] = i;
+                cur += 1;
+            }
+        }
+    }
+};
