0f4761bf17baf0e29099a4e454ea4427395c9b76
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-24 15:42:58 -0400

    Add: subarray-sum-equals-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> prefix_sum_cnt;
+
+        prefix_sum_cnt[0] = 1; // set initial cnt 1 for sum to 0
+        int accum_sum = 0; 
+        int res = 0;
+        for (auto& num: nums) {
+            accum_sum += num; // accumulated sum until num (follow index order)
+
+            // Find # ways for prefix to num - (prefix to index X) = k
+            // = Find # of prefix: prefix to num - k
+            if (prefix_sum_cnt.find(accum_sum - k) != prefix_sum_cnt.end()) {
+                // found match: update res
+                // always include the current num
+                //  -> search the number of ways to start the subarray and sum to k
+                res += prefix_sum_cnt[accum_sum - k];
+            } else {
+                // not found match: do nothing
+            }
+
+            // NOTICE: update prefix sum cnt "after" res update
+            if (prefix_sum_cnt.find(accum_sum) == prefix_sum_cnt.end()) {
+                prefix_sum_cnt[accum_sum] = 1;
+            } else {
+                prefix_sum_cnt[accum_sum] += 1;
+            }
+        }
+        return res;
+    }
+};
