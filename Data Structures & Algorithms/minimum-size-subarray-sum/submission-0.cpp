63aa0d4f0ad26a8d5643305840d3fa2dc8cce1ba
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-23 23:40:22 -0400

    Add: minimum-size-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int min_len = nums.size() + 1;
+
+        int window_sum = 0;
+        int l = 0;
+        for (int r = 0; r < nums.size(); r++) {
+            // update window with new r
+            window_sum += nums[r];
+            while (window_sum >= target) {
+                min_len = min(min_len, r - l + 1);
+                // move left pointer, update window sum
+                window_sum -= nums[l];
+                l += 1;
+            }
+        }
+
+        if (min_len == nums.size() + 1) {
+            return 0;
+        }
+
+        return min_len;
+    }
+};
