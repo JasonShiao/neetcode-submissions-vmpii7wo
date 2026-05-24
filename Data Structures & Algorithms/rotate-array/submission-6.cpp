1fff78bda1aa0be7536110ca4986c6420b468c23
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-24 15:48:29 -0400

    Add: rotate-array - submission-6

---
+++ Data Structures & Algorithms/rotate-array/submission-6.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    // void reverse(vector<int>& nums, int l, int r) {
+    //     while (l < r) {
+    //         int temp = nums[l];
+    //         nums[l] = nums[r];
+    //         nums[r] = temp;
+    //         l += 1;
+    //         r -= 1;
+    //     }
+    // }
+    void rotate(vector<int>& nums, int k) {
+        // Approach 1: in-place but brute-force O(nk): 
+        //     keep bubble the last element to the first
+        //     repeat for k times
+
+        // !!!Approach 2: O(1) space and O(n) time
+        //.  reverse whole, 
+        //.  reverse first k, 
+        //.  reverse the last n-k
+        // Notice: handle the case k > nums.size()
+        // reverse(nums, 0, nums.size() - 1);
+        // reverse(nums, 0, (k % nums.size()) - 1);
+        // reverse(nums, k % nums.size(), nums.size() - 1);
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + (k % nums.size()) );
+        reverse(nums.begin() + (k % nums.size()), nums.end());
+
+    }
+};
