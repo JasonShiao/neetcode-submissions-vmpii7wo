fef8e9ae6fc3d1c966cfe51b2d5bdb0d347f3174
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-27 19:41:26 -0400

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, unordered_set<int>> prereq_graph;
+        // build the graph
+        for (auto prereq: prerequisites) {
+            prereq_graph[prereq[0]].insert(prereq[1]);
+        }
+        
+        // Topological sort
+        vector<int> res;
+        unordered_set<int> visited_in_path; // used to detect cycle
+        // no built-in union find in C++, use a set
+        unordered_set<int> added; // used to reject duplicate in result
+
+        // post order dfs
+        function<bool(int)> dfs = [&](int i) {
+            if (visited_in_path.find(i) != visited_in_path.end()) {
+                return false;
+            }
+            if (added.find(i) != added.end()) {
+                return true; // early return is important!!
+            }
+
+            visited_in_path.insert(i); // mark for deeper layers
+            // process (insert) all neighbors
+            for (auto prereq: prereq_graph[i]) {
+                if (!dfs(prereq)) {
+                    return false;
+                }
+            }
+            
+            // Empty current prereq list to reject dup visit
+            prereq_graph[i] = unordered_set<int>{};
+
+            // then process (insert)
+            res.push_back(i);
+            added.insert(i);
+
+            visited_in_path.erase(i); // recover for backtracking
+
+            return true;
+        };
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i)) {
+                return vector<int>{};
+            }
+        }
+
+        return res;
+    }
+};
