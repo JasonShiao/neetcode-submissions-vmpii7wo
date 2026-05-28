433db3c1ed109a10c544641d464f6facabb2630c
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-05-28 08:17:26 -0400

    Add: is-subsequence - submission-4

---
+++ Data Structures & Algorithms/is-subsequence/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        // time: O(n^2)
+        for (int i = 0; i < t.length(); i++) {
+            // new start position
+            int s_cursor = 0;
+            int t_cursor = i;
+            
+            while (s_cursor < s.length() && 
+                    t_cursor < t.length()) {
+                if (s[s_cursor] == t[t_cursor]) {
+                    s_cursor++;
+                }
+                t_cursor++;
+            }
+            if (s_cursor == s.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
