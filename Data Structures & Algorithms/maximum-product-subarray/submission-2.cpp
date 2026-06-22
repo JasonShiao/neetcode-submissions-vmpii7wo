22d7b9be5585c3c1e7022449ea47ded15f132772
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-06-21 22:37:40 -0700

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        // Iterate from 1 to nums.size()
+        // maintain min and max product of subarray that **includes num**
+
+        int res = INT_MIN;
+
+        int maxProd = 1;
+        int minProd = 1;
+
+        for (auto num: nums) {
+            // to get max -> 3 cases
+            int tmpMax = max(maxProd * num, minProd * num);
+            tmpMax = max(tmpMax, num);
+            // to get min -> 3 cases
+            int tmpMin = min(maxProd * num, minProd * num);
+            tmpMin = min(tmpMin, num);
+
+            maxProd = tmpMax;
+            minProd = tmpMin;
+
+            // Update in every iter
+            res = max(res, maxProd);
+
+            // Handle 0 here
+            if (num == 0) {
+                maxProd = 1;
+                minProd = 1;
+            }
+        }
+
+        return res;
+    }
+};
