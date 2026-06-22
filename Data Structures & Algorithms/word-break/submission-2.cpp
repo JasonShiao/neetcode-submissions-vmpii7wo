ccd910986b45ecd1c975422c7b25a610495a0b5f
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-06-22 00:50:43 -0700

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool dfs(const string& s, int idx, 
+                vector<string>& wordDict, 
+                unordered_set<int>& checked) {
+        if (idx == s.length()) {
+            return true;
+        }
+
+        if (checked.find(idx) != checked.end()) {
+            return false;
+        }
+
+        // Check all branches (words in dict)
+        for (auto word: wordDict) {
+            if (s.substr(idx, word.length()) == word) {
+                if (dfs(s, idx + word.length(), wordDict, checked)) {
+                    return true;
+                } else {
+                    // mark as checked
+                    checked.insert(idx+word.length());
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // Like a puzzle game but with inf number of each piece
+
+        // DFS + DP
+        // NOTE: DP is used for filtering out some duplicate cases
+
+        unordered_set<int> checked;
+        return dfs(s, 0, wordDict, checked);
+    }
+};
