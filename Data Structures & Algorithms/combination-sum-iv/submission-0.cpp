4a946efcb18d78459b178f882d8c31afbea58d91
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-06-22 21:08:32 -0700

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // return the number of comb for target
+    int orderedCombinations(vector<int>& nums, 
+        unordered_map<int, int>& dp, int target) {
+
+        if (target < 0) {
+            return 0;
+        } else if (target == 0) {
+            return 1;
+        }
+        
+        int res = 0;
+        for (auto num: nums) {
+            if (dp.find(target - num) == dp.end()) {
+                int new_comb = orderedCombinations(nums, dp, target - num);
+                dp[target - num] = new_comb;
+            }
+            res += dp[target - num];
+        }
+
+        return res;
+    }
+
+    int combinationSum4(vector<int>& nums, int target) {
+        unordered_map<int, int> dp; // <target, num of ordered comb>
+        return orderedCombinations(nums, dp, target);
+    }
+};
