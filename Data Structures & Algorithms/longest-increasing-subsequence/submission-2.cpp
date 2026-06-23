c5867c5533ba1bcc734388001992a09040fc327f
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-06-22 17:15:58 -0700

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        // Dynamic Programming from back
+
+        unordered_map<int, int> lis; // the longest subsequence that INCLUDES i
+        lis[nums.size() - 1] = 1;
+        int maxLIS = 1;
+        // O(n^2)
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            int curLIS = 1;
+            // iterate through each of latter lis
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] < nums[j]) {
+                    curLIS = max(curLIS, lis[j] + 1);
+                }
+            }
+            // update the result
+            lis[i] = curLIS;
+            // update maxLIS on the fly
+            if (maxLIS < curLIS) {
+                maxLIS = curLIS;
+            }
+        }
+
+        return maxLIS;
+    }
+};
