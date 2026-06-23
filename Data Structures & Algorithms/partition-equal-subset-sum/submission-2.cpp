3588e52be2ea2c23fb0dc71f80f72862dfa13842
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-06-22 20:20:20 -0700

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        // target = total/2
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if (total % 2) {
+            return false;
+        }
+
+        int target = total / 2;
+
+        // unordered_set<int> possible_sum;
+        // possible_sum.insert(0); // this is critical
+        // for (auto num: nums) {
+        //     unordered_set<int> new_possible_sum;
+        //     for (auto t: possible_sum) {
+        //         new_possible_sum.insert(t + num);
+        //         new_possible_sum.insert(t);
+        //     }
+        //     possible_sum = new_possible_sum;
+        // }
+
+        // if (possible_sum.find(target) == possible_sum.end()) {
+        //     return false;
+        // } else {
+        //     return true;
+        // }
+
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        // Complexity: O(n * sum(nums))
+        for (int num : nums) {
+            for (int sum = target; sum >= num; sum--) {
+                // select or not select
+                dp[sum] = dp[sum] || dp[sum - num];
+            }
+
+            if (dp[target]) return true;
+        }
+
+        return dp[target];
+
+    }
+};
