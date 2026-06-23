13b52346087d42c919c3f68f38d24e2da310c44a
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-06-23 02:32:01 -0700

    Add: perfect-squares - submission-2

---
+++ Data Structures & Algorithms/perfect-squares/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        // NOTICE: Use vector over map to improve performance!!
+        //unordered_map<int, int> dp; // i -> least number of squares for i
+        vector<int> dp(n+1, 0);
+
+        vector<int> sq_list;
+        // WARNING: start from 1 instead of 0 !!!!
+        for (int i = 1; i <= n; i++) {
+            if (i * i <= n) {
+                sq_list.push_back(i*i);
+            } else {
+                break;
+            }
+        }
+
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int least_num_sq = INT_MAX;
+            for (auto sq: sq_list) {
+                if (i - sq >= 0) {
+                    least_num_sq = min(least_num_sq, dp[i - sq] + 1);
+                }
+            }
+            dp[i] = least_num_sq;
+        }
+
+        return dp[n];
+    }
+};
