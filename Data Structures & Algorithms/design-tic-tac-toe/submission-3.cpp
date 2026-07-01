a45e312ccc78f5d4c7190da5a01fef7d9e591608
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-06-30 17:21:53 -0700

    Add: design-tic-tac-toe - submission-3

---
+++ Data Structures & Algorithms/design-tic-tac-toe/submission-3.cpp
@@ -0,0 +1,80 @@
+class TicTacToe {
+private:
+    int n_;
+    vector<vector<int>> board_;
+public:
+    // Approach 2: use counters: rows, cols, diag, antidiag
+    // player 1: cnt += 1;
+    // player 2: cnt -= 1;
+    // if abs(cnt) == n -> win
+
+    TicTacToe(int n) {
+        n_ = n;
+        board_ = vector<vector<int>>(n, vector<int>(n, -1)); // -1 for empty
+    }
+    
+    int move(int row, int col, int player) {
+        // Tips: only need to check cells related to the current move
+        board_[row][col] = player;
+
+        // check all rows with col
+        bool win_detected = true;
+        for (int r = 0; r < n_; r++) {
+            if (board_[r][col] != player) {
+                win_detected = false;
+                break;
+            }
+        }
+        if (win_detected) {
+            return player;
+        }
+
+        win_detected = true;
+        // check all cols with row
+        for (int c = 0; c < n_; c++) {
+            if (board_[row][c] != player) {
+                win_detected = false;
+                break;
+            }
+        }
+        if (win_detected) {
+            return player;
+        }
+
+        // check diagonal ()
+        if (row == col) {
+            win_detected = true;
+            for (int i = 0; i < n_; i++) {
+                if (board_[i][i] != player) {
+                    win_detected = false;
+                    break;
+                }
+            }
+            if (win_detected) {
+                return player;
+            }
+        }
+        if ((n_ - row - 1) == col) {
+            win_detected = true;
+            for (int i = 0; i < n_; i++) {
+                if (board_[n_-1-i][i] != player) {
+                    win_detected = false;
+                    break;
+                }
+            }
+            if (win_detected) {
+                return player;
+            }
+        }
+    
+
+        // no winner
+        return 0;
+    }
+};
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe* obj = new TicTacToe(n);
+ * int param_1 = obj->move(row,col,player);
+ */
