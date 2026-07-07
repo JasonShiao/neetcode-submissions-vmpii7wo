9077dab25daea5af491182bc543ef401d8f32787
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-07-06 17:23:12 -0700

    Add: longest-substring-with-at-most-two-distinct-characters - submission-1

---
+++ Data Structures & Algorithms/longest-substring-with-at-most-two-distinct-characters/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        unordered_map<char, int> window;
+        int res = 0;
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            window[s[r]] += 1;
+            if (window.size() > 2) { // more than 2 keys -> must remove one
+                // move l (and dec char cnt) until no dup
+                while (l < r && window.size() > 2) {
+                    window[s[l]] -= 1;
+                    if (window[s[l]] == 0) {
+                        window.erase(s[l]); // erase() for both map and set
+                    }
+                    l++;
+                }
+            }
+            res = max(res, r - l + 1);
+        }
+
+        return res;
+    }
+};
