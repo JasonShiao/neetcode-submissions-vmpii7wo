11b9678f1879d92591c85958327bf713846e70bf
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-07-14 20:25:57 -0700

    Add: 4sum - submission-5

---
+++ Data Structures & Algorithms/4sum/submission-5.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+
+        // 1. sort
+        sort(nums.begin(), nums.end());
+
+        // 2. Critical: How to reject (skip) duplicate?
+        for (int i = 0; i < nums.size() - 1; i++) {
+            // no dup check for first (i.e. i = 0)
+            // NOTE: After the loop, we've found all possibility starting with nums[i]
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            for (int j = i+1; j < nums.size() - 1; j++) {
+                // no dup check for first (i.e. j = i+1)
+                if (j > i+1 && nums[j] == nums[j - 1]) continue;
+
+                // Critical: handle integer overflow
+                long long partial_target = (long long)target - nums[i] - nums[j];
+
+                int k = j + 1;
+                int l = nums.size() - 1;
+                while (k < l) {
+                    if (nums[k] + nums[l] > partial_target) {
+                        l -= 1;
+                    } else if (nums[k] + nums[l] < partial_target) {
+                        k += 1;
+                    } else {
+                        res.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k += 1;
+                        l -= 1;
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+};
