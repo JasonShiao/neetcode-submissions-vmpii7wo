f5bd9560f3be66017516e809045a802676598a6a
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-07-14 22:01:04 -0700

    Add: simplify-path - submission-1

---
+++ Data Structures & Algorithms/simplify-path/submission-1.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string temp_pattern;
+        deque<string> path_stack; // either stack or deque
+        for (auto c: path) {
+            if (c == '.') {
+                temp_pattern += c;
+            } else if (c == '/') {
+                // process the temp_pattern
+                if (temp_pattern.length() == 0) {
+                    // do nothing
+                } else if (temp_pattern == ".") {
+                    // do nothing
+                } else if (temp_pattern == "..") {
+                    if (!path_stack.empty()) {
+                        path_stack.pop_back();
+                    }
+                } else {
+                    // a dir
+                    path_stack.push_back(temp_pattern);
+                }
+                // clear
+                temp_pattern = "";
+            } else {
+                temp_pattern += c;
+            }
+        }
+
+        // process & clean the last remaining pattern
+        if (temp_pattern.length() == 0) {
+            // do nothing
+        } else if (temp_pattern == ".") {
+            // do nothing
+        } else if (temp_pattern == "..") {
+            if (!path_stack.empty()) {
+                path_stack.pop_back();
+            }
+        } else {
+            // a dir
+            path_stack.push_back(temp_pattern);
+        }
+
+        // might need to reverse if using stack
+        // construct the simplified string
+        string res;
+        while (!path_stack.empty()) {
+            res += "/";
+            res.append(path_stack.front());
+            path_stack.pop_front();
+        }
+
+        if (res.empty()) {
+            res = "/";
+        }
+
+        return res;
+    }
+};
