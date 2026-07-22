443beb9e2521772ebf3ae788fd8268edd2852224
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-07-21 17:02:38 -0700

    Add: minimum-time-to-collect-all-apples-in-a-tree - submission-0

---
+++ Data Structures & Algorithms/minimum-time-to-collect-all-apples-in-a-tree/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minTimeRecursion(unordered_map<int, vector<int>>& adj_list, 
+                        vector<bool>& hasApple,
+                        int from, int curr) {
+        int res = 0;
+        for (auto adj: adj_list[curr]) {
+            if (adj == from) {
+                continue;
+            }
+            auto t = minTimeRecursion(adj_list, hasApple, 
+                                    curr, adj);
+            if (t > 0 || hasApple[adj]) {
+                res += (t + 2); // forward & backward: 2
+            }
+        }
+
+        return res;
+    }
+
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        // if there is apple in the left child or subtree of left child -> should search left
+        // similar to the right branch
+
+        // Create the adj list
+        unordered_map<int, vector<int>> adj_list;
+        for (auto edge: edges) {
+            adj_list[edge[0]].push_back(edge[1]);
+            adj_list[edge[1]].push_back(edge[0]);
+        }
+
+        return minTimeRecursion(adj_list, hasApple, -1, 0);
+    }
+};
