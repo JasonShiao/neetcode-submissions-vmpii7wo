1eec37802f5927a1fb070771cdc9bb7838ab8e49
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2026-08-17 18:04:44 -0700

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        // build a map from nums2
+        // find the first greater using stack
+
+        // O(n+m)
+
+        unordered_map<int, int> lookup;
+        stack<int> val_stack; // the stack is maintained in decreasing order
+        for (auto num: nums2) {
+            // check with stack and add to lookup
+            while (!val_stack.empty() && val_stack.top() < num) {
+                lookup[val_stack.top()] = num;
+                val_stack.pop();
+            }
+            // push to stack
+            val_stack.push(num);
+        }
+
+        // iterate through nums1
+        vector<int> res;
+        for (auto num: nums1) {
+            if (lookup.find(num) == lookup.end()) {
+                res.push_back(-1);
+            } else {
+                res.push_back(lookup[num]);
+            }
+        }
+        return res;
+    }
+};
